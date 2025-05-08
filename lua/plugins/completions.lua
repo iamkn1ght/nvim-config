393d5d3e56ad6240d63c729023e1599f343a029e
zacharias.priller <contact@zachpriller.com>
2025-05-08 14:12:09 +0200

    Copilot Added

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -11,6 +11,7 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		-- In deiner "hrsh7th/nvim-cmp" Plugin-Konfiguration
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
@@ -25,17 +26,34 @@ return {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
+				preselect = cmp.PreselectMode.Item, -- Wichtig für die Vorauswahl
+
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
-          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+					["<D-CR>"] = cmp.mapping.confirm({ select = false }), -- oder cmp.mapping.confirm()
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
+
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
+					{ name = "luasnip" },
 				}, {
 					{ name = "buffer" },
 				}),
